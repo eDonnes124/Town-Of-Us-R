f6d8784a48cbf3611e4651e03d99cc12dfa5631e
ItsTheNumberH <h-newton@hotmail.com>
2022-02-20 14:50:53 +0000

    Cheap fix for Flash

--- source/Patches/Roles/Modifiers/Flash.cs
+++ source/Patches/Roles/Modifiers/Flash.cs
@@ -5,7 +5,7 @@ namespace TownOfUs.Roles.Modifiers
 {
     public class Flash : Modifier, IVisualAlteration
     {
-        public static float SpeedFactor = 1.25f;
+        public static float SpeedFactor = 1f;
 
         public Flash(PlayerControl player) : base(player)
         {
--- source/VisualAppearance.cs
+++ source/VisualAppearance.cs
@@ -4,7 +4,7 @@ namespace TownOfUs
 {
     public class VisualAppearance
     {
-        public float SpeedFactor { get; set; } = 1.0f;
+        public float SpeedFactor { get; set; } = 0.8f;
         public Vector3 SizeFactor { get; set; } = new Vector3(0.7f, 0.7f, 1.0f);
 
     }
