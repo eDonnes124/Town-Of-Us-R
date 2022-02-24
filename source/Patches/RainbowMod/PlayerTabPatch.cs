09b93a19c9b1d4232763cba83f3219d4328b421a
slyde99 <99327542+slyde99@users.noreply.github.com>
2022-02-24 20:42:24 +0800

    Add rainbow effect to color selection

--- source/Patches/RainbowMod/PlayerTabPatch.cs
+++ source/Patches/RainbowMod/PlayerTabPatch.cs
@@ -6,20 +6,34 @@ using Object = UnityEngine.Object;
 
 namespace TownOfUs.RainbowMod
 {
-    [HarmonyPatch(typeof(PlayerTab), nameof(PlayerTab.OnEnable))]
-    public class PlayerTabPatch
+    [HarmonyPatch(typeof(PlayerTab))]
+    public static class PlayerTabPatch
     {
-        public static void Postfix(PlayerTab __instance)
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(PlayerTab.OnEnable))]
+        public static void OnEnablePostfix(PlayerTab __instance)
         {
             for (int i = 0; i < __instance.ColorChips.Count; i++)
             {
                 var colorChip = __instance.ColorChips[i];
-                colorChip.transform.localScale *= 0.8f;
-                var x = __instance.XRange.Lerp((i % 4) / 4f) + 0.25f;
-                var y = __instance.YStart - (i / 4) * 0.55f;
+                var x = __instance.XRange.Lerp(i % 5 / 4f);
+                var y = __instance.YStart - (i / 5) * 0.6f - 0.1f;
                 colorChip.transform.localPosition = new Vector3(x, y, -1f);
             }
+        }
 
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(PlayerTab.Update))]
+        public static void UpdatePostfix(PlayerTab __instance)
+        {
+            for (int i = 0; i < __instance.ColorChips.Count; i++)
+            {
+                if (RainbowUtils.IsRainbow(i))
+                {
+                    __instance.ColorChips[i].Inner.color = RainbowUtils.Rainbow;
+                    break;
+                }
+            }
         }
     }
 }
