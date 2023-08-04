ed40c80dc3ff2cad2013418b6a015abde0462a49
twix <91138333+whichtwix@users.noreply.github.com>
2023-08-04 23:28:47 +0600

    add online game check proceed needed in local game

--- source/Patches/ShowHostMeetingPatch.cs
+++ source/Patches/ShowHostMeetingPatch.cs
@@ -12,6 +12,8 @@ namespace TownOfUs.Patches
         
         public static void Postfix(MeetingHud __instance)
         {
+            if (AmongUsClient.Instance.NetworkMode != NetworkModes.OnlineGame) return;
+
             var host = GameData.Instance.GetHost();
 
             if (host != null)
@@ -26,6 +28,8 @@ namespace TownOfUs.Patches
         
         public static void Setup(MeetingHud __instance)
         {
+            if (AmongUsClient.Instance.NetworkMode != NetworkModes.OnlineGame) return;
+
             __instance.ProceedButton.gameObject.transform.localPosition = new(-2.5f, 2.2f, 0);
             __instance.ProceedButton.gameObject.GetComponent<SpriteRenderer>().enabled = false;
             __instance.ProceedButton.GetComponent<PassiveButton>().enabled = false;
