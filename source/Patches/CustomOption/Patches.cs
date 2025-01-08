239211d230af750f944368df3a732701a0c51043
le killer <141348096+lekillerdesgames@users.noreply.github.com>
2025-01-08 18:23:20 +0100

    Update Patches.cs
    Update PlayerJoinPatch and remove useless patch

--- source/Patches/CustomOption/Patches.cs
+++ source/Patches/CustomOption/Patches.cs
@@ -629,18 +629,6 @@ namespace TownOfUs.CustomOption
             }
         }
 
-        [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.RpcSyncSettings))]
-        private class PlayerControlPatch
-        {
-            public static void Postfix()
-            {
-                if (PlayerControl.AllPlayerControls.Count < 2 || !AmongUsClient.Instance ||
-                    !PlayerControl.LocalPlayer || !AmongUsClient.Instance.AmHost) return;
-
-                Rpc.SendRpc();
-            }
-        }
-
         [HarmonyPatch(typeof(PlayerPhysics), nameof(PlayerPhysics.CoSpawnPlayer))]
         private class PlayerJoinPatch
         {
@@ -649,7 +637,7 @@ namespace TownOfUs.CustomOption
                 if (PlayerControl.AllPlayerControls.Count < 2 || !AmongUsClient.Instance ||
                     !PlayerControl.LocalPlayer || !AmongUsClient.Instance.AmHost) return;
 
-                Rpc.SendRpc();
+                Rpc.SendTargetRpc(__instance.myPlayer);
             }
         }
 
